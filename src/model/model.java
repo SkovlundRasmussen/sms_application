e2fea403a66a22eb25bccab73ed411a736de5fa8
Toldam <nich3070@stud.kea.dk>
2017-04-04 10:03:37 +0200

    Connect to server commit.

--- src/model/model.java
+++ src/model/model.java
@@ -1,11 +1,85 @@
 package model;
 
+import java.sql.*;
 import java.util.List;
 
-/**
- * Created by krist on 31-03-2017.
- */
 public class model implements iModel {
+
+    public static void connectToBD()
+    {
+        String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://sql11.freemysqlhosting.net/sql11167340";
+
+        Connection conn = null;
+        Statement stmt = null;
+
+        try
+        {
+            //
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL);
+
+            //
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM users";
+            System.out.println(sql);
+            ResultSet rs = stmt.executeQuery(sql);
+
+/*            if (rs.next())
+            {
+                bResult = true;
+            }
+            else
+            {
+                bResult = false;
+            }*/
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }
+        catch(SQLException se)
+        {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+        finally
+        {
+            //finally block used to close resources
+            try
+            {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2)
+            {
+                // nothing we can do
+            }
+            try
+            {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se)
+            {
+                se.printStackTrace(); //end finally try
+            }
+        }
+        System.out.println("Goodbye!");
+        //return bResult;
+    }
+
+
+
     @Override
     public User getUser() {
         return null;
